26a842d92fe3ef8813384036e0daf0c6b0496961
hamzah-syed <hamzah.syed17@gmail.com>
2021-02-18 17:22:41 +0500

    infrastructure test added

--- backend/test/new.test.ts
+++ backend/test/new.test.ts
@@ -1,13 +1,30 @@
-import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
-import * as cdk from '@aws-cdk/core';
-import * as New from '../lib/new-stack';
+import {
+  expect,
+  matchTemplate,
+  MatchStyle,
+  haveResource,
+  SynthUtils,
+} from "@aws-cdk/assert";
 
-test('Empty Stack', () => {
-    const app = new cdk.App();
-    // WHEN
-    const stack = new New.NewStack(app, 'MyTestStack');
-    // THEN
-    expectCDK(stack).to(matchTemplate({
-      "Resources": {}
-    }, MatchStyle.EXACT))
+import * as cdk from "@aws-cdk/core";
+import * as New from "../lib/new-stack";
+
+const app = new cdk.App();
+const stack = new New.NewStack(app, "MyTestStack");
+
+test("dynamodb todosTable exist", () => {
+  expect(stack).to(
+    haveResource("AWS::DynamoDB::Table", {
+      TableName: "todosTable",
+    })
+  );
+});
+test("Api main lambda exist with following configration", () => {
+  expect(stack).to(
+    haveResource("AWS::Lambda::Function", {
+      FunctionName: "todoFn",
+      MemorySize: 1024,
+      Handler: "main.handler",
+    })
+  );
 });
